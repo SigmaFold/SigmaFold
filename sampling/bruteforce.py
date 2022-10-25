81d803769bbe9f9595bd03416c0dedb198e152d9
nicolasdhnr <njd20@ic.ac.uk>
2022-10-26 00:09:25 +0100

    Added Energy Computation

--- sampling/bruteforce.py
+++ sampling/bruteforce.py
@@ -24,7 +24,8 @@ def profile(fnc):
 
 
 # Take a list of H and Ps as input
-input_sequence = list(input("Enter the input sequence: "))
+# input_sequence = list(input("Enter the input sequence: "))
+input_sequence = "HPHHHPHHPH"
 print(input_sequence)
 
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
@@ -49,8 +50,11 @@ def generate_paths(x, y, visited, path):
 
 
 generate_paths(0, 0, visited, [])
-
 print("Total Possible Paths ====  > ", len(paths))
+
+# map each element in each path to the corresponding element in the input sequence
+paths = [list(zip(path, input_sequence)) for path in paths]
+print("Valid paths are ====> ", paths)
 
 
 # plot all points in path
@@ -59,7 +63,7 @@ print("Total Possible Paths ====  > ", len(paths))
 def get_neighbours(x, y):
     neighbours = []
     # create list of directions with diagonals
-    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]  # right, down, left, up
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
     for dir in dirs:
         new_x = x + dir[0]
         new_y = y + dir[1]
@@ -67,27 +71,59 @@ def get_neighbours(x, y):
     return neighbours
 
 
-for path in paths:
-    density = 0
-    for coordinate in path:
-        neighbours = get_neighbours(coordinate[0], coordinate[1])
-        for neighbour in neighbours:
-            if neighbour in path:
-                density += 1
-    path.insert(0, density)
+# sum all neighbours where second element of tuple is H
+def get_energy(path):
+    energy = 0
+    for point in path:
+        print(point)
+        if point[1] == 'H':
+            for neighbour in get_neighbours(point[0][0], point[0][1]):
+                index = path.index((neighbour, 'H')) if (neighbour, 'H') in path else -1
+                if index != -1:
+                    energy -= 1
 
-densities = [path[0] for path in paths]
+    return energy
 
-# print(densities)
-max_density = max(densities)
+
+# add energies to paths
+paths = [(get_energy(path), path) for path in paths]
+print("Paths with energies ====> ", paths)
+
+# isolate all paths with minimum energy
+min_energy = min([path[0] for path in paths])
+print("Minimum energy is ====> ", min_energy)
+min_energy_paths = [path for path in paths if path[0] == min_energy]
+# print("Minimum energy paths are ====> ", min_energy_paths)
+
+# Plot all paths with minimum energy
+# Plot Hs and Ps in a different colour
+# Plot all paths with minimum energy in a different plot
 
 import matplotlib.pyplot as plt
 
-# if density is greater or equal to 5 plot
-for path in paths:
-    if path[0] == max_density:
-        x = [coordinate[0] for coordinate in path[1:]]
-        y = [coordinate[1] for coordinate in path[1:]]
-        plt.scatter(x, y)
-        plt.plot(x, y)
-        plt.show()
+
+# plot in a different colour if H or P
+def plot_path(path):
+    # use np.where
+
+
+
+
+for path in min_energy_paths:
+    plot_path(path[1])
+
+
+#
+# # print(densities)
+# max_density = max(densities)
+#
+# import matplotlib.pyplot as plt
+#
+# # if density is greater or equal to 5 plot
+# for path in paths:
+#     if path[0] == max_density:
+#         x = [coordinate[0] for coordinate in path[1:]]
+#         y = [coordinate[1] for coordinate in path[1:]]
+#         plt.scatter(x, y)
+#         plt.plot(x, y)
+#         plt.show()
