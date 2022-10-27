f13488311d4894235980169b4f6d5b01395f06c9
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2022-10-27 13:09:52 +0100

    added plotting function
    edited handle_sequence() to return the stable paths as well
    added plot_sequences() to plot out stable structures

--- Sequence to Shape/path_generator.py
+++ Sequence to Shape/path_generator.py
@@ -2,13 +2,13 @@
 Generates every possible combination of H and Ps within a matrix of size 2n where n is the size of the input sequence.
 """
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from rules import bond_rules
 
 origin = (0,0)
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
 dirs_full = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]  # right, down, left, up + all the diagonals
-visited = set()
+visited = set() # locations that have been visited
 
 def generate_paths(x, y, visited, path, paths, n, sequence):
     """Recursive function
@@ -53,6 +53,7 @@ def get_energy(path, sequence):
 
 
 def handle_sequence(sequence):
+    stable_paths = []
     paths = []
     n = len(sequence)
     generate_paths(0, 0, visited, [], paths, n, list(sequence))
@@ -66,10 +67,14 @@ def handle_sequence(sequence):
     current_energy_min = min(energy_list)
     stability = energy_list.count(current_energy_min)
 
+    for i in range(len(energy_list)):
+        if energy_list[i] == current_energy_min:
+            stable_paths.append(paths[i])
+
     # Stability is the number of lowest energy configuration for a given sequence
     # The lower the stability indicator, the more stable the sequence is (uhmm)
     # print(stability)
-    return stability
+    return stability, stable_paths
 
     # if density is greater or equal to 5 plot
     # for path in paths:
--- Sequence to Shape/sampling_main.py
+++ Sequence to Shape/sampling_main.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
+
 from permutation_generator import perm_gen
 from path_generator import handle_sequence
 
 def get_optimal_sequence(length=1, model=2):
+    stable_sequence_list = []
     sequences_list = perm_gen(length, model)
     stabilty_list = []
     for sequence in sequences_list:
-        sequence_stability = handle_sequence(sequence)
+        [sequence_stability, stable_sequence_paths] = handle_sequence(sequence)
         stabilty_list.append(sequence_stability)
     optimal_stability = min(stabilty_list)
     print(stabilty_list.count(optimal_stability))
@@ -15,6 +18,21 @@ def get_optimal_sequence(length=1, model=2):
     for index in indices:
         print(sequences_list[index])
     print(f'Each of them has {optimal_stability} stable configurations')
+    print(stable_sequence_paths)
+    print(len(stable_sequence_paths))
+    return stable_sequence_paths
+
+
+def plot_sequences(stable_sequence_paths):
+    sequence_length = len(stable_sequence_paths[0])
+    fig, axs = plt.subplots(int(len(stable_sequence_paths) / 4), 4)
+    for i in range(0, int(len(stable_sequence_paths) / 4)):
+        for j in range(0, 4):
+            axs[i, j].plot(*zip(*stable_sequence_paths[(i * 4) + j]), marker='.')
+            # axs[i, j].grid()
+    plt.setp(axs, xlim=[-sequence_length, sequence_length], ylim=[-sequence_length, sequence_length])
+    plt.show()
 
 if __name__=='__main__':
-    get_optimal_sequence(12, 2)
+    stable_paths = get_optimal_sequence(4, 2)
+    plot_sequences(stable_paths)
