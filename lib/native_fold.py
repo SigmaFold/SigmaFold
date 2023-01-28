3910639e49e6241d15a439a3f23d1adf2f238436
Nicolas Dehandschoewercker <87447994+nicolasdhnr@users.noreply.github.com>
2023-01-28 22:03:17 +0100

    Optimised the heck oout of the brute force with bigman nabeel

--- lib/native_fold.py
+++ lib/native_fold.py
@@ -8,7 +8,7 @@ import heapq
 import cProfile, pstats, io
 import networkx as nx
 from math import ceil, floor
-
+import numpy as np
 def profile(fnc):
     
     """A decorator that uses cProfile to profile a function"""
@@ -30,23 +30,15 @@ def profile(fnc):
 @profile
 def native_fold(n):
     """Takes the previous n's path as input and attempts to add the next element in the sequence to the end of the path"""
-    # load paths json if it exists 
-    # try:
-    #     with open('data/paths.json') as json_file:
-    #         old_paths = rapidjson.loads(json_file)
-    #         # Convert every path to a list of tuples
-    #         if len(old_paths[0]) <= n:
-    #             old_paths = [[tuple(point) for point in path] for path in old_paths]
-    #         else:
-    #             old_paths = [] # the info stored is too big - start from scratch
-
-    # except:
-    #     old_paths = []
 
     dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]  
     paths = [] # new paths to be generated
     cap_x = ceil(n/2) -1 
-    def generate_paths(visited, path, idx=1):
+    visited = np.zeros((n, n), dtype=bool)
+    visited[0, 0] = True
+    visited[1, 0] = True
+
+    def generate_paths(path):
         """Generate all possible paths of length n starting at (0, 0)"""
         if len(path) == n:
             paths.append(path)
@@ -56,17 +48,17 @@ def native_fold(n):
             new_x = path[-1][0] + dir[0]
             new_y = path[-1][1] + dir[1]
             cap_y = ceil((n/(abs(new_x)+1)) - 1) if abs(new_x) > 0 else cap_x
-            #print("y", new_y)
-           # print("cap_x", cap_x)
-            if  abs(new_y) > cap_y or abs(new_x) > cap_x:
-               # print("exit")
-                continue
-            elif (new_x, new_y) not in visited:
-                visited.add((new_x, new_y))
-                generate_paths(visited, path + [(new_x, new_y)], idx+1)
-                visited.remove((new_x, new_y))
 
-    generate_paths(set([(0, 0), (0,1)]), [(0, 0), (0, 1)], 2) # start at (0, 1) to avoid double counting
+            if  abs(new_y) > cap_y or abs(new_x) > cap_x:
+                continue
+            
+            elif not visited[new_y, new_x]:
+                visited[new_y, new_x] = True
+                generate_paths(path + [(new_x, new_y)])
+                visited[new_y, new_x] = False
+
+
+    generate_paths([(0, 0), (0, 1)]) # start at (0, 1) to avoid double counting
     
     # Plot the paths
 
@@ -136,7 +128,7 @@ def generateWalks(n):
 
 
 if __name__ == "__main__":
-    n = 25
+    n = 16
     x = 0
     y = 0
     # G = generateWalks(n)
