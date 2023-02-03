5b3b4b086c77b7daff2d8af2affd1a30a3a9591f
Alexandre Péré <93291204+Ataraxa@users.noreply.github.com>
2023-02-03 22:17:00 +0000

    bug fix for ui

--- tweaking-algorithm/invenv/inv_env/__init__.py
+++ tweaking-algorithm/invenv/inv_env/__init__.py
@@ -4,3 +4,8 @@ register(
     id="inv_fold/PrimWorld-v0",
     entry_point="inv_env.envs:PrimitiveInverseEnv",
 )
+
+register(
+    id="inv_fold/TweakWorld-v0",
+    entry_point="inv_env.envs:TweakingInverse",
+)
