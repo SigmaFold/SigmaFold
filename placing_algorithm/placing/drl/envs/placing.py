8d17941bebcb0773e0670e1c6ec9edd92167992e
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2023-04-09 20:13:47 +0100

    completed placing, tetsing to be done

--- placing_algorithm/placing/drl/envs/placing.py
+++ placing_algorithm/placing/drl/envs/placing.py
@@ -4,12 +4,11 @@ from gym import spaces
 import os
 import sys
 import pygame
-import time
-import math
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
-from library.shape_helper import path_to_shape_numbered, deserialize_path, deserialize_point, deserialize_shape
-from library.db_query_templates import get_random_shape, get_all_sequences_for_shape, find_HP_assignments, get_all_random_shapes
+from library.db_query_templates import get_all_random_shapes
+from library.shape_helper import deserialize_path, deserialize_shape
+
 
 class Placing(gym.Env):
     """
@@ -23,59 +22,48 @@ class Placing(gym.Env):
         self.shapes = get_all_random_shapes(length)
         sample = self.shapes.sample(1)
         self.shape_id = sample.shape_id.iloc[0]
-        self.HP_matrix = deserialize_shape(self.shape_id)    # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
-        self.correct_sequence = sample.best_sequence.iloc[0]    # the correct sequence for the target shape as a string
-        self.path = deserialize_path(sample.optimal_path.iloc[0])   # path as list of tuples coordinates        
-        self.curr_pos = self.path[0]  # position in the target shape matrix as tuple
+        # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
+        self.HP_matrix = deserialize_shape(self.shape_id)
+        # the correct sequence for the target shape as a string
+        self.correct_sequence = sample.best_sequence.iloc[0]
+        # path as list of tuples coordinates
+        self.path = deserialize_path(sample.optimal_path.iloc[0])
+        # position in the target shape matrix as tuple
+        self.curr_pos = self.path[0]
 
-
-        self.curr_sequence = [] # where we will be storing hp assingments
-
+        self.curr_sequence = []  # where we will be storing hp assingments
 
         # Static attributes
         self.length = length
         self.max_attempts = max_attempts
-        self.render_mode = render_mode  # activates or deativates the UI. Activated if set to "human"
+        # activates or deativates the UI. Activated if set to "human"
+        self.render_mode = render_mode
         self.num_actions = 0
         self.attempts = 0
 
-        #TODO: limit position to be following the path
-        #TODO: add one hot encoding for observation
-
         self.action_space = spaces.MultiBinary(2)
-        self.observation_space = spaces.MultiBinary(10) # 4 neighbours + 2 HP assignments
-
-    # def generate_path(self, shape_id):
-    #     #TODO: make it so this doesnt call db at every reset
-    #     # get all sequences for the target shape
-    #     sequences = get_all_sequences_for_shape(shape_id)
-    #     # sort sequences by degeneracy in an ascending order and save the first one's sequence and path
-    #     sequences = sequences.sort_values(by=["degeneracy"], ascending=True)
-    #     best_sequence = sequences.iloc[0]
-    #     sequence = best_sequence["sequence"]
-    #     path = best_sequence["path"]
-    #     # convert path string into list of tuples
-    #     path = deserialize_path(path)
-    #     path_mat, HP_mat, _ = path_to_shape_numbered(
-    #         path, sequence)    # convert path into a numbered matrix
-    #     return path_mat, HP_mat, sequence
+        self.observation_space = spaces.MultiBinary(10)  # 4 neighbours + 2 HP assignments
 
     def reset(self, options=None, seed=None):
         # get the initial shape
         if self.attempts >= self.max_attempts:
             sample = self.shapes.sample(1)
             self.shape_id = sample.shape_id.iloc[0]
-            self.HP_matrix = deserialize_shape(self.shape_id)    # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
-            self.correct_sequence = sample.best_sequence.iloc[0]    # the correct sequence for the target shape as a string
-            self.path = deserialize_path(sample.optimal_path.iloc[0])   # path as list of tuples coordinates            
-            self.curr_pos = self.path[0]  # position in the target shape matrix as tuple
+            # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
+            self.HP_matrix = deserialize_shape(self.shape_id)
+            # the correct sequence for the target shape as a string
+            self.correct_sequence = sample.best_sequence.iloc[0]
+            # path as list of tuples coordinates
+            self.path = deserialize_path(sample.optimal_path.iloc[0])
+            # position in the target shape matrix as tuple
+            self.curr_pos = self.path[0]
             self.curr_sequence = []
             self.attempts = 0
         else:
             self.attempts += 1
-            self.HP_matrix = deserialize_shape(self.shape_id)    # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
+            # currently 0s and 1s, but adding H or P will correspond to 2 or 3 respectively
+            self.HP_matrix = deserialize_shape(self.shape_id)
             self.curr_sequence = []
-        
 
         if self.render_mode == "human":
             pygame.init()
@@ -100,30 +88,31 @@ class Placing(gym.Env):
         # updating the current sequence with the action
         actions_dict = {0: "H", 1: "P"}
         action = np.argmax(action)
-        action = actions_dict[action]
-        self.curr_sequence.append(action)
+        self.last_action = np.zeros((2, 1), dtype=np.int)
+        self.last_action[action] = 1    # H is 10, P is 01
+        residue = actions_dict[action]
+        self.curr_sequence.append(residue)
 
         # updating the HP matrix according to action
         pos_action_col, pos_action_row = self.curr_pos
-        assign_action = action
         assign_dict = {"H": 2, "P": 3}
-        self.HP_matrix[pos_action_row, pos_action_col] = assign_dict[assign_action]
+        self.HP_matrix[pos_action_row, pos_action_col] = assign_dict[residue]
 
         # updating the current position
         self.num_actions += 1
         self.curr_pos = self.path[self.num_actions]
-        
+
         # render
         if self.render_mode == "human":
-            self.render(pos_action_row, pos_action_col, assign_action)
-        
+            self.render(pos_action_row, pos_action_col, residue)
+
         # check reward
         reward, done = self.compute_reward(pos_action_row, pos_action_col)
         # get observation space
         obs = self._get_obs()
         return obs, reward, done, {}
 
-    def render(self, pos_action_row, pos_action_col, assign_action):
+    def render(self, pos_action_row, pos_action_col, residue):
         """
         Render the environment to the screen.
         """
@@ -134,7 +123,7 @@ class Placing(gym.Env):
                 sys.exit()
 
         # render the current pos as a green square overwriting the target shape
-        if assign_action == "H":
+        if residue == "H":
             pygame.draw.circle(self.shape_surface, (0, 255, 0), (
                 pos_action_col*self.cell_size, pos_action_row*self.cell_size), radius=self.cell_size/3)
         else:
@@ -148,38 +137,37 @@ class Placing(gym.Env):
         """
         Get the observation of the environment.
         """
-        # IMPORTANT, SET THE DATATYPE TO BE CORRECT HERE
-        obs = np.zeros(6, dtype=np.int8)
-        # get the neighbours of the current position
+        neighbour_vector = self.find_neighbours()
+        obs = np.vstack([self.last_action, neighbour_vector])
 
-        return obs
+        return obs.flatten()
 
     def compute_reward(self):
         """
-        If the folding matrix is the same as the target shape, then reward is 1.
+        If the current sequence is the same as the correct sequence then maximum reward.
+        For every wrong residue, it is penalised -2.
+        For every correct residue it is rewarded 0.1.
+        For entirely correct sequence, it is rewardede 10
+        olding matrix is the same as the target shape, then reward is 1.
         Else, reward is 0.
         """
-        reward = 0
-        done = False
-        # TODO: Compare the last element of the current sequence with the correct sequence
-        # # fail immediately if out of bounds
-        # if self.target_shape[pos_action_row, pos_action_col] == 0:
-        #     reward = -1
-        #     done = True
 
-        # # if any element is equal to -1 then something was placed out of bounds
-        # if self.num_actions == self.length:
-        #     reward_list = []
-        #     for correct_mat in self.correctHPassignments:
-        #         diff_matrix = correct_mat - self.HPassignments
-        #         # sum absolute values of all the elements in diff_matrix and normalise it
-        #         sum = np.sum(abs(diff_matrix)) / self.length
-        #         reward_list.append(((1-sum)-0.5)*2)
-        #     reward = max(reward_list)
-        #     done = True
+        reward = 0.1
+        done = False
+
+        # agent is allowed to assign all residues before ending. No premature end for wrong assignment
+        # penalise at every step
+        if self.curr_sequence[self.num_actions - 1] != self.correct_sequence[self.num_actions - 1]:
+            reward = -2
+
+        if self.num_actions >= self.length:
+            if "".join(self.curr_sequence) == self.correct_sequence:
+                reward = 10
+                done = True
+            else:
+                done = True
 
         return reward, done
-
 
     def find_neighbours(self):
         """
@@ -189,23 +177,23 @@ class Placing(gym.Env):
         10 - H
         11  - P
         """
-        neighbour_vector = np.zeros((8,1), dtype=int)
-   
-        # up, right, down and left dirs in matrix [m, n]
+        neighbour_vector = np.zeros((8, 1), dtype=int)
+
+        # up, right, down and left dirs in matrix [m, n]. Diagonals do not matter.
         dirs = np.array([[0, -1], [1, 0], [0, 1], [-1, 0]])
-            
-        curr_pos_list = list(self.curr_pos) # convert it to a list
-        
+
+        curr_pos_list = list(self.curr_pos)  # convert it to a list
+
         # fill the neighbour_vector appropriately
         for i, dir in enumerate(dirs):
             # get the neighbour in the given direction
             neighbour_pos = [x + y for x, y in zip(curr_pos_list, dir)]
             # check neighbour characteristic in shape
             neighbour_char = self.HP_matrix[neighbour_pos[1], neighbour_pos[0]]
-            if neighbour_char == 0: # if outside boundary, assign 00
+            if neighbour_char == 0:  # if outside boundary, assign 00
                 neighbour_vector[2*i] = 0
                 neighbour_vector[2*i+1] = 0
-            elif neighbour_char == 1: # in boundary but unassigned
+            elif neighbour_char == 1:  # in boundary but unassigned
                 # if in chain, assign 00
                 if self.num_actions == 0 and self.path[self.num_actions + 1] == tuple(neighbour_pos):
                     neighbour_vector[2*i] = 0
@@ -219,7 +207,7 @@ class Placing(gym.Env):
                 else:   # if not in chain, assign 01
                     neighbour_vector[2*i] = 0
                     neighbour_vector[2*i+1] = 1
-            elif neighbour_char == 2: # H
+            elif neighbour_char == 2:  # H
                 # if in chain, assign 00
                 if self.num_actions == 0 and self.path[self.num_actions + 1] == tuple(neighbour_pos):
                     neighbour_vector[2*i] = 0
@@ -233,7 +221,7 @@ class Placing(gym.Env):
                 else:   # if not in chain, assign 10
                     neighbour_vector[2*i] = 1
                     neighbour_vector[2*i+1] = 0
-            elif neighbour_char == 3: # P
+            elif neighbour_char == 3:  # P
                 # if in chain, assign 00
                 if self.num_actions == 0 and self.path[self.num_actions + 1] == tuple(neighbour_pos):
                     neighbour_vector[2*i] = 0
@@ -247,12 +235,12 @@ class Placing(gym.Env):
                 else:   # if not in chain, assign 11
                     neighbour_vector[2*i] = 1
                     neighbour_vector[2*i+1] = 1
-        
+
         return neighbour_vector
-    
+
 
 if __name__ == "__main__":
-    env  = Placing(16)
+    env = Placing(16)
     print(env.path)
     print(env.starting_pos)
     print(env.starting_dir)
