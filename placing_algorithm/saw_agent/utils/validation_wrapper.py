bac0d3bbd94b336c4b6d9289e9ef8ddbd289fa01
Ataraxa <alexandre_pere2033@yahoo.com>
2023-04-11 22:36:03 +0200

    fixed bug in ValidationMonitor

--- placing_algorithm/saw_agent/utils/validation_wrapper.py
+++ placing_algorithm/saw_agent/utils/validation_wrapper.py
@@ -10,13 +10,14 @@ class ValidationMonitor(gym.Wrapper):
         super().__init__(env)
         self.required_timesteps_dict = dict()
         self.temp_counter = 0
-
+    
     def step(self, action): # Still old step API, no truncation yet
         obs, reward, terminated, info = self.env.step(action)
-        self.temp_counter += 1
         if terminated:
             if info["is_cleared"]:
                 self.required_timesteps_dict[self.env.shape_id] = self.temp_counter
-            self.temp_counter = 0
+                self.temp_counter = 0
+            else:
+                self.temp_counter +=1
 
         return obs, reward, terminated, info
