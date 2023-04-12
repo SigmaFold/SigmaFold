8fdf680f5b1c52a1b03ed3f8fbfd25c4c69f6cfa
nicolasdhnr <njd20@ic.ac.uk>
2023-04-12 22:38:01 +0100

    extrapolation data

--- placing_algorithm/placing_agent/placing_validation.py
+++ placing_algorithm/placing_agent/placing_validation.py
@@ -9,7 +9,7 @@ from library.db_query_templates import get_validation_dataset
 from utils.validation_wrapper import ValidationMonitor
 
 def validation_testing(path, env):
-    validation_set = get_validation_dataset(16)
+    validation_set = get_validation_dataset()
     print("Trying to clear validation set of shape", validation_set.shape)
     env = gym.make(env,
                    render_mode=None,
@@ -36,15 +36,15 @@ def validation_testing(path, env):
             states = None
         
     
-    with open('./data_placing/validation_data_extrapolation.json', 'w') as outfile:
-        data = model.get_env().envs[0].required_timesteps_dict
-        data = {str(k): v for k, v in data.items()}
-        json.dump(data, outfile)
+        with open('./data_placing/validation_data_interpolation.json', 'w') as outfile:
+            data = model.get_env().envs[0].required_timesteps_dict
+            data = {str(k): v for k, v in data.items()}
+            json.dump(data, outfile)
 
-    with open('./data_placing/extra_data_extrapolation.json', 'w') as outfile:
-        data = model.get_env().envs[0].unfinished_shapes_dict
-        data = {str(k): v for k, v in data.items()}
-        json.dump(data, outfile)
+        with open('./data_placing/extra_data_interpolation.json', 'w') as outfile:
+            data = model.get_env().envs[0].unfinished_shapes_dict
+            data = {str(k): v for k, v in data.items()}
+            json.dump(data, outfile)
 
 if __name__=='__main__':
-    validation_testing('./models/extrapolation_test/extrapolation_fov_1_n_14', 'PlacingValidation-v0') # replace with model
+    validation_testing('./models/neighbour_test/fov_1_test/', 'PlacingValidation-v0') # replace with model
